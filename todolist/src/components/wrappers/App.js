9511b7c4e8269a2111ac566ecbd19cfaa30d3189
zuwannn <suwanneethongtha@gmail.com>
2021-09-01 23:40:34 +0700

    Add todo style

--- todolist/src/components/wrappers/App.js
+++ todolist/src/components/wrappers/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="container">
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/wrappers/App';
-// Add bootstrap 
 
-import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+// Add bootstrap 
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+// Add CSS style
+import './assets/style/index.css'
+
 
 ReactDOM.render(
   <App />,
