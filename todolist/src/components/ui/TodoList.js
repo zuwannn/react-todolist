0f253eb613c50569a2248d3e0cde6f0e0f25b529
zuwannn <suwanneethongtha@gmail.com>
2021-09-02 00:09:03 +0700

    Extract another component for TodoItem

--- todolist/src/components/ui/TodoList.js
+++ todolist/src/components/ui/TodoList.js
@@ -1,24 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function TodoList(props){
-    const {title, items} = props 
-    return(
-        <div className="todolist">
-          <h1>{title.toUpperCase()}</h1>
-          <ul className="list-unstyled">
-            {items.map(item => (
-              <li className="ui-state-default" key={item.id}>
-                <div className="checkbox">
-                  <label>
-                    <input type="checkbox" value=""/>
-                    {item.text}
-                  </label>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-    )
+function TodoList(props) {
+  const { title, items } = props;
+  return (
+    <div className="todolist">
+      <h1>{title.toUpperCase()}</h1>
+      <ul className="list-unstyled">
+        {items.map((item) => (
+          <TodoItem data={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default TodoList
+function TodoItem(props) {
+  const { data } = props;
+
+  return (
+    <li className="ui-state-default" key={data.id}>
+      <div className="checkbox">
+        <label>
+          <input type="checkbox" value="" />
+          {data.text}
+        </label>
+      </div>
+    </li>
+  );
+}
+
+export default TodoList;
