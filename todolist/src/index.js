fa7e7b8c9a4fb5560ecb91d40cecf8ec39761709
zuwannn <suwanneethongtha@gmail.com>
2021-09-01 18:22:55 +0700

    React Hello World

---
+++ todolist/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/style/index.css';
+import App from './components/wrappers/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
