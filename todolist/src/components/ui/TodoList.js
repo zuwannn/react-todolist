00ccfe6d878ef22e971a50b5376c1372b0b19d64
zuwannn <suwanneethongtha@gmail.com>
2021-09-02 14:41:21 +0700

    Seprate component for Header as well

--- todolist/src/components/ui/TodoList.js
+++ todolist/src/components/ui/TodoList.js
@@ -1,11 +1,12 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import Header from "../Header";
 
 function TodoList(props) {
   const { title, items } = props;
   return (
     <div className="todolist">
-      <h1>{title.toUpperCase()}</h1>
+      <Header title={title}/>
       <ul className="list-unstyled">
           {items.map(item => 
             <TodoItem key={item.id} data={item}/>
