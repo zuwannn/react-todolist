443ace910abb3fdb8e17c1da194e8c4d967ff4c4
zuwannn <suwanneethongtha@gmail.com>
2021-09-03 14:21:45 +0700

    Add a Filter component

--- todolist/src/components/Footer.js
+++ todolist/src/components/Footer.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import Filter from "./Filter";
 
 export default function Footer(props) {
-    const {count} = props
-    return(
-        <div className="todo-footer">
-            <strong>
-                <span className="count-todos">{count}</span>
-            </strong>
-            {' items left'}
-        </div>
-    )
+  const { count } = props;
+  return (
+    <div className="todo-footer clearfix">
+      <div className="pull-left">
+        <strong>
+          <span className="count-todos">{count}</span>
+        </strong>
+        {" items left"}
+      </div>
+      <div className="pull-right">
+          <Filter />
+      </div>
+    </div>
+  );
 }
