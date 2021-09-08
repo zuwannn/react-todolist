563d131d24a883d7cc21fc739ff565c269c0fc76
zuwannn <suwanneethongtha@gmail.com>
2021-09-08 15:34:41 +0700

    Create FilteredList.js

---
+++ todolist/src/components/FilteredList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoItem from '../components/ui/TodoItem'
+
+export default function FilteredList(props) {
+    const {items, changeStatus} = props
+
+    return(
+        <ul className="list-unstyled">
+            {items.map(item => (
+                <TodoItem key={item.id} data={item} changeStatus={changeStatus} />
+            ))}
+        </ul>
+    )
+}
