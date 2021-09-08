fde0c110d8fd6bfe7359d369d8ea299be258d6b8
zuwannn <suwanneethongtha@gmail.com>
2021-09-08 22:49:00 +0700

    Simplify things a little bit

--- todolist/src/components/wrappers/StateProvider.js
+++ todolist/src/components/wrappers/StateProvider.js
@@ -11,7 +11,7 @@ class StateProvider extends Component{
             query: '',
             mode: MODE_CREATE,
             filter: FILTER_ALL,
-            items: getAll()
+            list: getAll()
         }
     }
 
@@ -24,9 +24,9 @@ class StateProvider extends Component{
     }
 
     addNew(text) {
-        let updatedList = addToList(this.state.items, {text, completed: false});
+        let updatedList = addToList(this.state.list, {text, completed: false});
     
-        this.setState({items: updatedList})
+        this.setState({list: updatedList})
     }
 
     changeFilter(filter) {
@@ -34,9 +34,9 @@ class StateProvider extends Component{
     }
 
     changeStatus(itemId, completed) {
-        const updatedList = updateStatus(this.state.items, itemId, completed);
+        const updatedList = updateStatus(this.state.list, itemId, completed);
     
-        this.setState({items: updatedList});
+        this.setState({list: updatedList});
     }
     changeMode(mode = MODE_NONE){
         this.setState({mode})
